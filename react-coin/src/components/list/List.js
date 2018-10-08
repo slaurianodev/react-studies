b7d5473fc22c2e0c381bbaaf52c2879f8eae78f3
sharecarebr-sergiolauriano <sergio.lauriano@sharecare.com>
2018-10-08 17:25:59 -0300

    Components, state and props

--- react-coin/src/components/list/List.js
+++ react-coin/src/components/list/List.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
-import './Table.css';
 import Loading from '../common/Loading';
+import Table from './Table';
 
 class List extends React.Component{
 	constructor(){
@@ -49,38 +49,10 @@ class List extends React.Component{
 			return <div className='error'>{this.state.error}</div>
 		}
 		return(
-			<div className='Table-container'>
-				
-				<table className='Table'>
-					<thead className='Table-head'>
-						<tr>
-							<th>Cryptocurrency</th>
-							<th>Price</th>
-							<th>Market Cap</th>
-							<th>24H Change</th>
-						</tr>
-					</thead>
-					<tbody className='Table-body'>
-						{currencies.map((currency) =>(
-							<tr key={currency.id}>
-								<td>
-									<span className='Table-rank'>{currency.rank}</span>
-									{currency.name}
-								</td>
-								<td>
-									<span className='Table-dollar'>$ {currency.price}</span>
-								</td>
-								<td>
-									<span className='Table-rank'>$ {currency.marketCap}</span>
-								</td>
-								<td>
-									{this.renderChangePercent(currency.percentChange24h)}
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
+			<Table 
+				currencies={currencies} 
+				renderChangePercent={this.renderChangePercent}
+			/>
 		);
 	}
 }
